8635a7acc4b4502a75c424ebe97c05e5a8610994
Dongyue Zhang <zhangdongyue22@gmail.com>
2026-01-18 12:16:25 -0500

    changed paths

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,8 +2,8 @@ export const environment = {
     production: true,
     googleMapKey: process.env['GOOGLE_MAP_KEY'],
     basePath: process.env['API_BASE_PATH'],
-    facilityPath: '/facilities',
-    typePath: '/types',
-    activityPath: '/activities',
-    categoryPath: '/categories',
+    facilityPath: 'facilities',
+    typePath: 'types',
+    activityPath: 'activities',
+    categoryPath: 'categories',
 };
