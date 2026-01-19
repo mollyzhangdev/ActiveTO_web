f614b120ad4cc4a6ba326989ac52f8c9e114f764
Dongyue Zhang <zhangdongyue22@gmail.com>
2026-01-19 13:08:06 -0500

    changed base path

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
     googleMapKey: process.env['GOOGLE_MAP_KEY'],
-    basePath: 'https://api.activeto.mollyzhang.dev/',
+    basePath: 'https://api.activeto.mollyzhang.dev/toronto/',
     facilityPath: 'facilities',
     typePath: 'types',
     activityPath: 'activities',
